edb63df2aa4ad171ffd6ea2957e5fd33731e16a4
Kritarth Bijalwann <155350188+KayBe05@users.noreply.github.com>
2025-03-25 22:06:33 +0530

    Update constant.js

--- utils/constant.js
+++ utils/constant.js
@@ -1,1 +1,10 @@
+import { technologies } from './technologies';
+import { certifications } from './certifications';
+import { detailedProjects, projectCategories } from './projects';
 
+export {
+  technologies,
+  certifications,
+  detailedProjects,
+  projectCategories
+};
